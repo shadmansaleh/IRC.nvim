2900f4a90c7ac79c5906eb140c1a245ee1be2336
shadmansaleh <shadmansaleh3@gmail.com>
2021-05-01 12:12:32 +0600

    Improve numarics handling

--- lua/nirc/display.lua
+++ lua/nirc/display.lua
@@ -11,50 +11,51 @@ function display.open_view()
       local prompt_win_no = display.data.prompt_win.win
       if prompt_win_no and vim.api.nvim_win_is_valid(prompt_win_no) then
         vim.api.nvim_set_current_win(prompt_win_no)
+        vim.cmd('silent! startinsert')
         return true
       end
     end
-  else
-    -- create layout
-    vim.api.nvim_exec([[
-    silent! tabnew
-    silent! setlocal splitbelow
-    silent! setlocal nonumber
-    silent! setlocal norelativenumber
-    silent! setlocal autoread
-    silent! setlocal syn=irc
-    silent! setlocal linebreak
-    silent! setlocal breakindent
-    silent! setlocal breakindentopt=shift:31
-    silent! setlocal breakat&
-    silent! setlocal buftype=nofile
-    silent! call nvim_buf_set_name(0, 'IRC_preview')
-    silent! 5split
-    silent! enew
-    " silent! setlocal backspace=start,indent
-    silent! setlocal nonumber
-    silent! setlocal buftype=nofile
-    silent! setlocal bufhidden=hide
-    silent! setlocal norelativenumber
-    silent! setlocal virtualedit=onemore
-    silent! call nvim_buf_set_name(0, 'IRC_prompt')
-    silent! inoremap <silent><buffer> <CR> <cmd>lua require("nirc.keymaps").send_msg()<CR>
-    ]], false)
-    display.data = {
-      tab_no = vim.api.nvim_get_current_tabpage(),
-      prompt_win = {
-        buf = vim.api.nvim_get_current_buf(),
-        win = vim.api.nvim_get_current_win(),
-      }
-    }
-    vim.cmd('silent! wincmd k')
-    display.data.preview_win = {
+  end
+  -- create layout
+  vim.api.nvim_exec([[
+  silent! tabnew
+  silent! setlocal splitbelow
+  silent! setlocal nonumber
+  silent! setlocal norelativenumber
+  silent! setlocal autoread
+  silent! setlocal syn=irc
+  silent! setlocal linebreak
+  silent! setlocal breakindent
+  silent! setlocal breakindentopt=shift:31
+  silent! setlocal breakat&
+  silent! setlocal buftype=nofile
+  silent! call nvim_buf_set_name(0, 'IRC_preview')
+  silent! 5split
+  silent! enew
+  " silent! setlocal backspace=start,indent
+  silent! setlocal nonumber
+  silent! setlocal buftype=nofile
+  silent! setlocal bufhidden=hide
+  silent! setlocal norelativenumber
+  silent! setlocal virtualedit=onemore
+  silent! call nvim_buf_set_name(0, 'IRC_prompt')
+  silent! inoremap <silent><buffer> <CR> <cmd>lua require("nirc.keymaps").send_msg()<CR>
+  ]], false)
+  display.data = {
+    tab_no = vim.api.nvim_get_current_tabpage(),
+    prompt_win = {
       buf = vim.api.nvim_get_current_buf(),
       win = vim.api.nvim_get_current_win(),
     }
-    vim.api.nvim_set_current_win(display.data.prompt_win.win)
-  end
+  }
+  vim.cmd('silent! wincmd k')
+  display.data.preview_win = {
+    buf = vim.api.nvim_get_current_buf(),
+    win = vim.api.nvim_get_current_win(),
+  }
+  vim.api.nvim_set_current_win(display.data.prompt_win.win)
   vim.cmd('silent! startinsert')
+  return true
 end
 
 return display
--- lua/nirc/irc/handlers.lua
+++ lua/nirc/irc/handlers.lua
@@ -11,6 +11,100 @@ local handlers = {}
 
 local protocol = require'nirc.irc.protocol'
 
+local numerics = {
+  ['001'] = 'RPL_WELCOME',
+  ['002'] = 'RPL_YOURHOST',
+  ['003'] = 'RPL_CREATED',
+  ['004'] = 'RPL_MYINFO',
+  ['005'] = 'RPL_ISUPPORT',
+  ['010'] = 'RPL_BOUNCE',
+  ['221'] = 'RPL_UMODEIS',
+  ['251'] = 'RPL_LUSERCLIENT',
+  ['252'] = 'RPL_LUSEROP',
+  ['253'] = 'RPL_LUSERUNKNOWN',
+  ['254'] = 'RPL_LUSERCHANNELS',
+  ['255'] = 'RPL_LUSERME',
+  ['256'] = 'RPL_ADMINME',
+  ['257'] = 'RPL_ADMINLOC1',
+  ['258'] = 'RPL_ADMINLOC2',
+  ['259'] = 'RPL_ADMINEMAIL',
+  ['263'] = 'RPL_TRYAGAIN',
+  ['265'] = 'RPL_LOCALUSERS',
+  ['266'] = 'RPL_GLOBALUSERS',
+  ['276'] = 'RPL_WHOISCERTFP',
+  ['300'] = 'RPL_NONE',
+  ['301'] = 'RPL_AWAY',
+  ['302'] = 'RPL_USERHOST',
+  ['303'] = 'RPL_ISON',
+  ['305'] = 'RPL_UNAWAY',
+  ['306'] = 'RPL_NOWAWAY',
+  ['311'] = 'RPL_WHOISUSER',
+  ['312'] = 'RPL_WHOISSERVER',
+  ['313'] = 'RPL_WHOISOPERATOR',
+  ['314'] = 'RPL_WHOWASUSER',
+  ['317'] = 'RPL_WHOISIDLE',
+  ['318'] = 'RPL_ENDOFWHOIS',
+  ['319'] = 'RPL_WHOISCHANNELS',
+  ['321'] = 'RPL_LISTSTART',
+  ['322'] = 'RPL_LIST',
+  ['323'] = 'RPL_LISTEND',
+  ['324'] = 'RPL_CHANNELMODEIS',
+  ['329'] = 'RPL_CREATIONTIME',
+  ['331'] = 'RPL_NOTOPIC',
+  ['332'] = 'RPL_TOPIC',
+  ['333'] = 'RPL_TOPICWHOTIME',
+  ['341'] = 'RPL_INVITING',
+  ['346'] = 'RPL_INVITELIST',
+  ['347'] = 'RPL_ENDOFINVITELIST',
+  ['348'] = 'RPL_EXCEPTLIST',
+  ['349'] = 'RPL_ENDOFEXCEPTLIST',
+  ['351'] = 'RPL_VERSION',
+  ['353'] = 'RPL_NAMREPLY',
+  ['366'] = 'RPL_ENDOFNAMES',
+  ['367'] = 'RPL_BANLIST',
+  ['401'] = 'ERR_NOSUCHNICK',
+  ['402'] = 'ERR_NOSUCHSERVER',
+  ['403'] = 'ERR_NOSUCHCHANNEL',
+  ['404'] = 'ERR_CANNOTSENDTOCHAN',
+  ['405'] = 'ERR_TOOMANYCHANNELS',
+  ['421'] = 'ERR_UNKNOWNCOMMAND',
+  ['422'] = 'ERR_NOMOTD',
+  ['432'] = 'ERR_ERRONEUSNICKNAME',
+  ['433'] = 'ERR_NICKNAMEINUSE',
+  ['441'] = 'ERR_USERNOTINCHANNEL',
+  ['442'] = 'ERR_NOTONCHANNEL',
+  ['443'] = 'ERR_USERONCHANNEL',
+  ['451'] = 'ERR_NOTREGISTERED',
+  ['461'] = 'ERR_NEEDMOREPARAMS',
+  ['462'] = 'ERR_ALREADYREGISTERED',
+  ['464'] = 'ERR_PASSWDMISMATCH',
+  ['465'] = 'ERR_YOUREBANNEDCREEP',
+  ['471'] = 'ERR_CHANNELISFULL',
+  ['472'] = 'ERR_UNKNOWNMODE',
+  ['473'] = 'ERR_INVITEONLYCHAN',
+  ['474'] = 'ERR_BANNEDFROMCHAN',
+  ['475'] = 'ERR_BADCHANNELKEY',
+  ['481'] = 'ERR_NOPRIVILEGES',
+  ['482'] = 'ERR_CHANOPRIVSNEEDED',
+  ['483'] = 'ERR_CANTKILLSERVER',
+  ['491'] = 'ERR_NOOPERHOST',
+  ['501'] = 'ERR_UMODEUNKNOWNFLAG',
+  ['502'] = 'ERR_USERSDONTMATCH',
+  ['670'] = 'RPL_STARTTLS',
+  ['691'] = 'ERR_STARTTLS',
+  ['723'] = 'ERR_NOPRIVS',
+  ['900'] = 'RPL_LOGGEDIN',
+  ['901'] = 'RPL_LOGGEDOUT',
+  ['902'] = 'ERR_NICKLOCKED',
+  ['903'] = 'RPL_SASLSUCCESS',
+  ['904'] = 'ERR_SASLFAIL',
+  ['905'] = 'ERR_SASLTOOLONG',
+  ['906'] = 'ERR_SASLABORTED',
+  ['907'] = 'ERR_SASLALREADY',
+  ['908'] = 'RPL_SASLMECHS',
+
+}
+
 local buffer_text = {}
 
 -- Temporary solution for testing
@@ -23,6 +117,34 @@ local function show(buf_nr, ...)
     end
   end
   vim.api.nvim_buf_set_lines(buf_nr, 0, -1, false, buffer_text)
+end
+
+-- var to store partial commands
+local chunk_buffer = ''
+
+-- Handles responses from the server
+function handlers.responce_handler(client, err, chunk)
+  if not err and chunk then
+    chunk = chunk_buffer .. chunk
+    local lines = vim.split(chunk, '\r\n')
+    if not vim.endswith(chunk, '\r,\n') then
+      chunk_buffer = lines[#lines] or ''
+      table.remove(lines, #lines)
+    end
+    for _, line in pairs(lines) do
+      if #line > 1 then
+        local responce = protocol.parse_msg(line)
+        local cmd = responce.cmd
+        if pcall(tonumber(cmd)) and numerics[cmd] then
+          cmd = numerics[cmd]
+        end
+        if handlers[cmd] then
+          handlers[cmd](client, responce)
+        end
+        handlers.default_handler(client, responce)
+      end
+    end
+  end
 end
 
 -- Gets called for all events
@@ -39,9 +161,17 @@ function handlers.PING(client, responce)
 end
 
 -- Handle Nick name already in use
-handlers['433'] = function(client, _)
+function handlers.ERR_NICKNAMEINUSE(client, _)
   client.config.nick = '_'..client.config.nick
   client:send_cmd("nick", client.config.nick)
+  if not client.handshake_done then
+    client:send_cmd('user', client.config.username, client.config.username)
+  end
+end
+
+-- Handlers wellcome
+function handlers.RPL_WELCOME(client, _)
+  client.handshake_done = true
 end
 
 function handlers.ERROR(client, responce)
@@ -57,48 +187,6 @@ function handlers.post_connect(client)
   end
   client:send_cmd('nick', client.config.nick)
   client:send_cmd('user', client.config.username, client.config.username)
-end
-
-local function handshake(client, responce)
-  -- we are accepted
-  if responce.cmd == '001' then
-    return true
-  --username already in use
-  elseif responce.cmd == '433' then
-    client.config.nick = '_'..client.config.nick
-    client:send_cmd("nick", client.config.nick)
-    client:send_cmd('user', client.config.username, client.config.username)
-    return false
-  -- Ping from server or anything else
-  elseif handlers[responce.cmd] then
-    handlers[responce.cmd](client, responce)
-    return false
-  end
-end
-
-local chunk_buffer = ''
-
--- Handles responses from the server
-function handlers.responce_handler(client, err, chunk)
-  if not err and chunk then
-    chunk = chunk_buffer .. chunk
-    local lines = vim.split(chunk, '\r\n')
-    if not vim.endswith(chunk, '\r,\n') then
-      chunk_buffer = lines[#lines] or ''
-      table.remove(lines, #lines)
-    end
-    for _, line in pairs(lines) do
-      if #line > 1 then
-        local responce = protocol.parse_msg(line)
-        if not client.config.handshake_done then
-          client.config.handshake_done = handshake(client, responce)
-        elseif handlers[responce.cmd] then
-          handlers[responce.cmd](client, responce)
-        end
-        handlers.default_handler(client, responce)
-      end
-    end
-  end
 end
 
 return handlers
