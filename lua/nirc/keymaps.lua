c5007d34dbf073f1fcf6ec634a787f007f917216
shadmansaleh <shadmansaleh3@gmail.com>
2021-05-05 00:50:21 +0600

    Allow multiline messages internaly

--- lua/nirc/keymaps.lua
+++ lua/nirc/keymaps.lua
@@ -4,12 +4,13 @@ local utils = require'nirc.utils'
 
 function keymaps.send_msg()
   local nirc_data = require'nirc.data'
-  local line = vim.api.nvim_get_current_line()
+  local lines = vim.api.nvim_buf_get_lines(nirc_data.display.prompt_win.buf, 0, -1, false)
   local prompt = utils.get_prompt()
-  line = utils.remove_prompt(line, prompt)
-  nirc_data.clients[nirc_data.active_client]:prompt(line)
-  vim.api.nvim_del_current_line()
-  vim.api.nvim_buf_set_lines(nirc_data.display.prompt_win.buf, 0, 0, false, {prompt})
+  lines[1] = utils.remove_prompt(lines[1], prompt)
+  for _, line in pairs(lines) do
+    nirc_data.clients[nirc_data.active_client]:prompt(line)
+  end
+  vim.api.nvim_buf_set_lines(nirc_data.display.prompt_win.buf, 0, -1, false, {prompt})
   vim.api.nvim_win_set_cursor(0, {1, #prompt - 1})
 end
 
